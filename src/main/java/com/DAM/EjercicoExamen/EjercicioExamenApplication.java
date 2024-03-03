f692956ad0c13b078a0524dc6ea52f962483d0f9
daprea <123488781+daprea@users.noreply.github.com>
2024-03-03 12:37:46 +0100

    Add files via upload

---
+++ src/main/java/com/DAM/EjercicoExamen/EjercicioExamenApplication.java
@@ -0,0 +1,13 @@
+package com.DAM.EjercicoExamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioExamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DAM/EjercicoExamen/EjercicioExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DAM.EjercicoExamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
